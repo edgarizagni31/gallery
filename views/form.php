0affa1b5fd90127ec4694d98fd288e6a69d56290
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-27 22:40:40 -0500

    styles for the page

--- views/form.php
+++ views/form.php
@@ -3,28 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ee726eb0d9.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/form.css">
     <title>Formulario</title>
 </head>
 <body>
-    
-    <form 
-        action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);   ?>" 
-        method="post"
-        enctype="multipart/form-data"
-    >
-        <input type="file" name="image">
-        <input type="text" name="title">
-        <input type="text" name="desc">
-        <input type="submit" value="Subir">
-    </form>
-    <div>
-        <?php  
-            if ( empty($errors) ) {
-                echo "<div><p>Todo correcto</p></div>";
-            } else  {
-                show_errors($errors);
-            }
+    <div class="container">
+        <button class="container__button">
+            <a href="../index.php">Ver galeria</a>
+        </button>
+        <form 
+            action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);   ?>" 
+            method="post"
+            enctype="multipart/form-data"
+            class="form"
+        >
+            <input 
+                type="file" 
+                name="image" 
+                class="form__file"
+            >
+            <input 
+                type="text" 
+                name="title" 
+                autocomplete="off" 
+                class="form__input"
+                placeholder="Titulo"
+            >
+            <input 
+                type="text" 
+                name="desc" 
+                autocomplete="off" 
+                class="form__input"
+                placeholder="Descripción"
+            >
+            <button type="submit" class="form__btn">
+                <div class="form__content">
+                    <i class="fas fa-cloud-upload-alt icon"></i>
+                    <p>Subir foto</p> 
+                </div>
+            </button>
+        </form>
+        <section class="content-result">
+            <?php  
+                if ($errors) {
+                    echo "<ul class='error-list'>".$errors."</ul>";
+                }
+
+                if ( $insert ) {
+                    echo "<p class='success'>Foto subida exitosamente</p>";
+                }
         ?>
-    </div> 
+        </section> 
+    </div>
 </body>
 </html>
--- views/gallery.php
+++ views/gallery.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ee726eb0d9.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles/gallery.css">
     <title>Galeria</title>
 </head>
 <body>
-    <button><a href="controllers/form.php">Subir</a></button>
+    <button class="button">
+        <i class="fas fa-upload icon"></i>
+        <a href="controllers/form.php">Subir</a>
+    </button>
+    <h1>Galeria</h1>
+    <section class="gallery">
+       <?php  
+        show_photos();
+       ?> 
+    </section>
 </body>
 </html>
